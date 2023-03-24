301193530d8414c0f2e4f2be6024c37f7c331641
Grigory Rechistov <grigory.rechistov@gmail.com>
2023-03-24 23:27:19 +0100

    Make parse_header return struct

--- parse.cpp
+++ parse.cpp
@@ -3,12 +3,13 @@
 // correctly.
 #include "parse.h"
 #include "exc.h"
+#include "header.h"
 
 
 static constexpr uint32_t magic_sec_ms = 0xA1B2C3D4;
 static constexpr uint32_t magic_sec_ns = 0xA1B23C4D;
 
-void parse_header( ReadDword dword_reader, FILE *f) {
+ParsedHeader parse_header( ReadDword dword_reader, FILE *f) {
     uint32_t magic = dword_reader(f);
     if (magic != magic_sec_ms && magic != magic_sec_ns) {
         throw BadMagic();
@@ -23,4 +24,7 @@ void parse_header( ReadDword dword_reader, FILE *f) {
     }
     uint32_t fcs_f_link_type = dword_reader(f);
 
+    ParsedHeader res{};
+    res.is_time_in_ns = magic == magic_sec_ns;
+    return res;
 }
--- parse.h
+++ parse.h
@@ -1,10 +1,11 @@
 #pragma once
 #include <cstdio>
 #include <cstdint>
-#include<functional> 
+#include<functional>
+#include "header.h" 
 
 using ReadDword = std::function <uint32_t (FILE* stream)>;
 
 
 
-void parse_header( ReadDword dword_reader, FILE *f);
+ParsedHeader parse_header( ReadDword dword_reader, FILE *f);
