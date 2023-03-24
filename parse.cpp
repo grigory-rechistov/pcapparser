5c9d4785e8c6ded6b72ff843583d065b80557ee1
Grigory Rechistov <grigory.rechistov@gmail.com>
2023-03-24 20:33:58 +0100

    Add failing test case

---
+++ parse.cpp
@@ -0,0 +1,1 @@
+// File format after https://datatracker.ietf.org/doc/id/draft-gharris-opsawg-pcap-00.html
--- test.cpp
+++ test.cpp
@@ -1,7 +1,27 @@
 // unit tests
+#include "parse.h"
+#include <cstdio>
+#include <cstdlib>
 
+#define FAIL(reason) do {\
+    printf("Expectation failed at %s:%d %s\n", __FILE__, __LINE__, (reason)); \
+    std::exit(-1); \
+} while (0)
+
+class TruncatedInput {};
+
+static void test_parse_header_on_empty() {
+    try {
+        parse_header({});
+    } catch (const TruncatedInput) {
+
+    } catch (...) {
+        FAIL("Unexpected exception thrown");
+    }
+    FAIL("Should not return normally on empty header");
+}
 
 int main() {
-
+    test_parse_header_on_empty();
     return 0;
 }
