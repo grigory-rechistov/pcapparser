a92e163ba70146f413d900d08b61429b3a8c7982
Grigory Rechistov <grigory.rechistov@gmail.com>
2023-03-24 21:47:18 +0100

    Rename variable, add second magic

--- parse.cpp
+++ parse.cpp
@@ -3,13 +3,14 @@
 #include "exc.h"
 
 
-static constexpr uint32_t magic_sec_ms = 0xA1B2C3D4; 
+static constexpr uint32_t magic_sec_ms = 0xA1B2C3D4;
+static constexpr uint32_t magic_sec_ns = 0xA1B23C4D;
 
 void parse_header( ReadDword dword_reader, FILE *f) {
-    uint32_t header = dword_reader(f);
-    if (header != magic_sec_ms) {
+    uint32_t magic = dword_reader(f);
+    if (magic != magic_sec_ms && magic != magic_sec_ns) {
         throw BadMagic();
     }
 
-    (void)header;
+    (void)magic;
 }
