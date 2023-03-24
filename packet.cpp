d023ff9eff5eb739acf0d78f4b71b6663aff9eee
Grigory Rechistov <grigory.rechistov@gmail.com>
2023-03-25 00:53:10 +0100

    Convert failed read to incomplete

--- packet.cpp
+++ packet.cpp
@@ -1,6 +1,16 @@
 #include "packet.h"
+#include "bele.h"
+#include "exc.h"
 
 
 void PacketRecord::parse(ReadDword dword_reader, FILE *f) {
-    
+
+    try {
+        uint32_t full_seconds = reorder_u32(dword_reader(f));
+        uint32_t sub_seconds = reorder_u32(dword_reader(f)); // to be decoded ms/ns
+        uint32_t captured_length = reorder_u32(dword_reader(f));
+        uint32_t original_length = reorder_u32(dword_reader(f));
+    } catch (const TruncatedInput &e) {
+        incomplete = true;
+    }
 }
