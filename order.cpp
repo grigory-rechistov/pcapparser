7d45c58e635ba50cbb58a6463ba50af9f4001382
Grigory Rechistov <grigory.rechistov@gmail.com>
2023-03-25 21:10:23 +0100

    Process SBE

--- order.cpp
+++ order.cpp
@@ -12,11 +12,18 @@ OrderBookSnapshot::OrderBookSnapshot(const std::vector<uint8_t> &payload):
 
     const auto p = payload.data();
 
-    // Borderline undefined behavior ensues
+    // Borderline undefined behavior with dereferencing ensues
+    // Market data packet header
     MsgSeqNum = *reinterpret_cast<const uint32_t*>(p);
     MsgSize = *reinterpret_cast<const uint16_t*>(p + 4);
     MsgFlags = *reinterpret_cast<const uint16_t*>(p + 6);
     SendingTime = *reinterpret_cast<const uint64_t*>(p + 8);
+
+    // SBE header
+    BlockLength = *reinterpret_cast<const uint16_t*>(p + 16);
+    TemplateId = *reinterpret_cast<const uint16_t*>(p + 18);
+    SchemaId = *reinterpret_cast<const uint16_t*>(p + 20);
+    Version = *reinterpret_cast<const uint16_t*>(p + 22);
 
     valid = true;
 }
@@ -34,13 +41,21 @@ std::string OrderBookSnapshot::dump() {
     char buf[1000] = {0};
 
     snprintf(buf, sizeof(buf) -1, "OrderBookSnapshot:\n"
-                       "  Marker data packet header\n"
+                       "  Market data packet header\n"
                        "    MsgSeqNum %#x\n"
                        "    MsgSize %#x\n"
                        "    MsgFlags %#x\n"
                        "    SendingTime %ld\n"
                        "  SBE Header\n"
+                       "    BlockLength %#x\n"
+                       "    TemplateId %#x\n"
+                       "    SchemaId %#x\n"
+                       "    Version %#x\n"
                        , MsgSeqNum, (unsigned)MsgSize, (unsigned)MsgFlags,
-                         SendingTime);
+                         SendingTime,
+                         (unsigned)BlockLength,
+                         (unsigned)TemplateId, (unsigned)SchemaId, 
+                         (unsigned)Version
+    );
     return std::string(buf);
 }
--- order.h
+++ order.h
@@ -10,6 +10,11 @@ class OrderBookSnapshot {
     uint16_t MsgFlags;
     uint64_t SendingTime;
 
+    uint16_t BlockLength;
+    uint16_t TemplateId;
+    uint16_t SchemaId;
+    uint16_t Version;
+
     public:
     OrderBookSnapshot() = delete;
     OrderBookSnapshot(const std::vector<uint8_t> &payload);
